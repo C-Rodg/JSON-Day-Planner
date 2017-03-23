809dbebce681a97aec4ded69638e72f9bfa98df3
C-Rodg <rodgersbc@gmail.com>
2017-03-23 13:51:34 -0700

    Setup settings reducer

---
+++ src/reducers/reducer_settings.js
@@ -0,0 +1,15 @@
+// Import Action types
+
+// Set initial settings state
+
+const INITIAL_STATE = {
+    schedule : {}
+};
+
+export const settings = ( state = INITIAL_STATE, action) => {
+    switch(action.type) {
+
+        default: 
+            return state;
+    }
+}
