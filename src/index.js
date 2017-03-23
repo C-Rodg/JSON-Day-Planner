b5173ee8e595d299566722d4f19d21a2a87078b4
C-Rodg <rodgersbc@gmail.com>
2017-03-23 14:30:35 -0700

    Cleaned up history imports

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import 'babel-polyfill';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { hashHistory } from 'react-router';
-import { createBrowserHistory } from 'history';
 import { syncHistoryWithStore } from 'react-router-redux';
 
 import Root from './components/Root';
