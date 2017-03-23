3bd45c29a5520c3aaa03e2e0260b387ee9bb721e
C-Rodg <rodgersbc@gmail.com>
2017-03-23 13:42:39 -0700

    Setup page holders

---
+++ src/components/SchedulePage.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const SchedulePage = () => {
+    return (
+        <div class="schedule-page">
+            Schedule Page
+        </div>
+    );
+};
+
+export default SchedulePage;
